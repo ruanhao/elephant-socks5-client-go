e2dba36d04dc3037368eba8882b0c311fe57c663
Hao Ruan <haoru+bitbucket@cisco.com>
2026-02-13 19:17:22 +0800

    feat: add cross-platform build script

---
+++ build_for_all_platforms.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Description:
+
+
+set -e
+PLATFORMS=("linux/amd64" "linux/arm64" "darwin/amd64" "darwin/arm64" "windows/amd64" "windows/arm64")
+OUTPUT_NAME="elephant"
+
+for PLATFORM in "${PLATFORMS[@]}"; do
+    IFS="/" read -r GOOS GOARCH <<< "$PLATFORM"
+    OUTPUT_FILE="${OUTPUT_NAME}_${GOOS}_${GOARCH}"
+    if [ "$GOOS" == "windows" ]; then
+        OUTPUT_FILE+=".exe"
+    fi
+    echo "Building for $GOOS/$GOARCH..."
+    env GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="-s -w" -o "$OUTPUT_FILE" .
+    echo "Created artifact: $OUTPUT_FILE"
+done
+
